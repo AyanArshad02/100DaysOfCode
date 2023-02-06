ec1147c5818c8766f868531c48787ab52d5e904d
Md Ayan Arshad <116447704+AyanArshad02@users.noreply.github.com>
2023-02-06 23:13:55 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,6 @@
+print("Hello New Coders")
+print("""
+This is how you can print multi-line statement.
+In this series, we are going to learn python from basic to advance in just 100 days with the help of CodeWithHarry-CWH.
+Learn to show-up daily and you will become an expert in python programming 
+""")
---
+++ Day-5.py
@@ -0,0 +1,16 @@
+# print("Hey I am a good boy. \n And this viewer is also a good boy/girl.") 
+# #\n is a escape sequence character i.e. \n ke baad jo print hoga woh next line mein hoga
+# '''
+# This
+# Is
+# A
+# Multi-line
+# Comment
+# OR directly comment likh kr har ek line ko select krke ctrl+/ type krege tw comment hojyega selected line
+# '''
+# print("AYAN","ARSHAD",2, sep="~") #sep="separator" jb ek print statement mein multiple cheez likhte hai comma de kar tb woh har ek ke baad separator laga dega
+# print("Ayan","Arshad",end="02/01/2002\n")
+# print("abcd")
+
+#My practice
+print("This is how \" double quotes\" executes inside double quotes")
